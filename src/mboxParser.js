a81bc4cef0c6ce00118737118bb6cabb9bd30920
Andre Adriano <andre@dev-next.com>
2019-08-08 13:40:16 -0300

    ♻️ Small code ajustments

--- src/mboxParser.js
+++ src/mboxParser.js
@@ -14,8 +14,8 @@ module.exports = (mboxPath, attachmentPath, pattern = false) => {
   let count = 0;
   let countFile = 0;
 
-  const loadingMessage = () => `${chalk.bold('mbox-attachment-extract')}
-  ${chalk.cyan('EMAIL:')} ${count}
+  const loadingMessage = (emailRead = 0) => `${chalk.bold('mbox-attachment-extract')}
+  ${chalk.cyan('EMAIL:')} ${emailRead}
   ${chalk.cyan('ATTACHMENTS:')} ${countFile}
   `;
   const loader = ora(loadingMessage());
@@ -25,43 +25,46 @@ module.exports = (mboxPath, attachmentPath, pattern = false) => {
   }
 
   console.clear();
-  mbox.on('message', async (message) => {
-    loader.start(loadingMessage());
+  mbox.on('message', async function messageParser(message) {
+    count = this.messageCount;
+    loader.start(loadingMessage(count));
 
-    count += 1;
-    const parsedMessage = await simpleParser(message);
+    try {
+      const parsedMessage = await simpleParser(message);
+      if (parsedMessage.attachments.length === 0) {
+        return true;
+      }
 
-    if (parsedMessage.attachments.length === 0) {
+      const attachment = parsedMessage.attachments.filter((att) => {
+        if (!fileFilter) return !!att.filename;
+        const canBeExtracted = att.filename && att.filename.match(fileFilter);
+        return canBeExtracted;
+      });
+
+      if (attachment.length === 0) {
+        return true;
+      }
+
+      attachment.forEach(writeAttachment(parsedMessage, attachmentPath));
+      countFile += attachment.length;
+
       return true;
+    } catch (err) {
+      this.emit('error', err);
+      return false;
     }
-
-    const attachment = parsedMessage.attachments.filter((att) => {
-      if (!fileFilter) return !!att.filename;
-
-      const canBeExtracted = att.filename && att.filename.match(fileFilter);
-      return canBeExtracted;
-    });
-
-    if (attachment.length === 0) {
-      return true;
-    }
-
-    attachment.forEach(writeAttachment(parsedMessage, attachmentPath));
-    countFile += attachment.length;
-
-    return true;
   });
 
 
   mbox.on('end', () => {
     loader.succeed(`${chalk.green.bold('All files extracted!')}
-    Total email parsed: ${count}
-    Total files extracted: ${countFile}`);
+  Total email parsed: ${chalk.bold(count)}
+  Total files extracted: ${chalk.bold(countFile)}
+    `);
     process.exit(0);
   });
 
   mbox.on('error', (err) => {
-    console.log('got an error', err);
     loader.fail(`${chalk.redBright.bold('Error: ')} ${err}`);
     process.exit(1);
   });
