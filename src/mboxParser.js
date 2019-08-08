13d50c49de9b60abaf63560b4e2729d5546870e9
Andre Adriano <andre@dev-next.com>
2019-08-08 11:33:23 -0300

    💄 Created better loading message

--- src/mboxParser.js
+++ src/mboxParser.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { simpleParser } = require('mailparser');
 const Mbox = require('node-mbox');
+const ora = require('ora');
+const chalk = require('chalk');
 
 const writeAttachment = require('./writeAttachment');
 
@@ -12,14 +14,16 @@ module.exports = (mboxPath, attachmentPath, pattern = false) => {
   let count = 0;
   let countFile = 0;
 
-  mbox.on('message', async (message) => {
-    count += 1;
-    console.clear();
-    console.log('===============================');
-    console.log(`EMAIL: ${count}`);
-    console.log(`ATTACHMENTS: ${countFile}`);
-    console.log('===============================');
+  const loadingMessage = () => ` ${chalk.bold('mbox-attachment-extract')}
+  ${chalk.cyan('EMAIL:')} ${count}
+  ${chalk.cyan('ATTACHMENTS:')} ${countFile}
+  `;
+  const loader = ora(loadingMessage());
 
+  mbox.on('message', async (message) => {
+    loader.start(loadingMessage());
+
+    count += 1;
     const parsedMessage = await simpleParser(message);
 
     if (parsedMessage.attachments.length === 0) {
@@ -45,7 +49,15 @@ module.exports = (mboxPath, attachmentPath, pattern = false) => {
 
 
   mbox.on('end', () => {
-    console.log('DONE!');
+    loader.succeed(`${chalk.green.bold('All files extracted!')}
+    Total email parsed: ${count}
+    Total files extracted: ${countFile}`);
     process.exit(0);
+  });
+
+  mbox.on('error', (err) => {
+    console.log('got an error', err);
+    loader.fail(`${chalk.redBright.bold('Error: ')} ${err}`);
+    process.exit(1);
   });
 };
