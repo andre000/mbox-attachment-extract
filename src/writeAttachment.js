8e3bdbe1edfa3d2cfa1f81ab1337e95a7dcdc954
Andre Adriano <andre@dev-next.com>
2019-08-08 13:59:06 -0300

    🐛 Fixing instances where the email has no subject

--- src/writeAttachment.js
+++ src/writeAttachment.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 module.exports = (parsedMessage, attachmentPath) => (file, i) => {
   const ext = path.extname(file.filename);
   const from = parsedMessage.from.text.match(/@(.+?)\./);
-  const filename = `${from[1]}_${parsedMessage.subject.replace(/\W+|\.+/g, '_')}_${i}`.length > 200
-    ? `${from[1]}_${parsedMessage.subject.replace(/\W+|\.+/g, '_')}_${i}`.substr(0, 200)
-    : `${from[1]}_${parsedMessage.subject.replace(/\W+|\.+/g, '_')}_${i}`;
+  const subject = parsedMessage.subject ? parsedMessage.subject.replace(/\W+|\.+/g, '_') : '_';
+
+  const filename = `${from[1]}_${subject.replace(/\W+|\.+/g, '_')}_${i}`.length > 200
+    ? `${from[1]}_${subject.replace(/\W+|\.+/g, '_')}_${i}`.substr(0, 200)
+    : `${from[1]}_${subject.replace(/\W+|\.+/g, '_')}_${i}`;
 
   fs.writeFileSync(`${attachmentPath}${filename}${ext}`, file.content);
 };
